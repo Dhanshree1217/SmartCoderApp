9b97764c73213a2fb54f16bc6f01e04f30f776b0
dhanashree <dhaytarkardhanshri12@gmail.com>
2026-03-17 03:42:33 +0530

    what you changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 import 'screens/home_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'auth_database.dart';
 
 void main() {
   runApp(SmartCoder());
@@ -13,28 +13,16 @@ class SmoothPageRoute<T> extends PageRouteBuilder<T> {
       : super(
           pageBuilder: (context, animation, secondaryAnimation) => page,
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            final fade = Tween<double>(begin: 0.0, end: 1.0).animate(
-              CurvedAnimation(parent: animation, curve: Curves.easeInOut),
-            );
-            final slide = Tween<Offset>(
-              begin: Offset(0.05, 0),
-              end: Offset.zero,
-            ).animate(CurvedAnimation(parent: animation, curve: Curves.easeInOut));
             return FadeTransition(
-              opacity: fade,
-              child: SlideTransition(position: slide, child: child),
+              opacity: CurvedAnimation(parent: animation, curve: Curves.easeInOut),
+              child: child,
             );
           },
-          transitionDuration: Duration(milliseconds: 400),
+          transitionDuration: Duration(milliseconds: 350),
         );
 }
 
 class SmartCoder extends StatelessWidget {
-  Future<bool> checkAutoLogin() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('isLoggedIn') ?? false;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -46,37 +34,8 @@ class SmartCoder extends StatelessWidget {
           primary: Color(0xFFFF8C42),
           secondary: Color(0xFFFFB380),
         ),
-        pageTransitionsTheme: PageTransitionsTheme(
-          builders: {
-            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-            TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-          },
-        ),
       ),
-      home: FutureBuilder<bool>(
-        future: checkAutoLogin(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-          
-          if (snapshot.data == true) {
-            return FutureBuilder<String>(
-              future: SharedPreferences.getInstance().then((prefs) => prefs.getString('userName') ?? 'User'),
-              builder: (context, nameSnapshot) {
-                if (nameSnapshot.hasData) {
-                  return HomeScreen(userName: nameSnapshot.data!);
-                }
-                return SplashScreen();
-              },
-            );
-          }
-          
-          return SplashScreen();
-        },
-      ),
+      home: SplashScreen(),
     );
   }
 }
